e315b058e8814e67f7dba9ba1daca52fc74c40eb
DeryaKesgin <99399615+DeryaKesgin@users.noreply.github.com>
2024-08-15 17:21:49 +0300

    commitnew

--- src/pages/ProtectedRoute.js
+++ src/pages/ProtectedRoute.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Navigate } from 'react-router-dom';
-
+//test
 const ProtectedRoute = ({ component: Component, ...rest }) => {
     const isLoggedIn = !!localStorage.getItem('patientId'); // Giriþ yapýldýðýný kontrol edin
 
